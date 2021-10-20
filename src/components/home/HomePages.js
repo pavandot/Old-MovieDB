30f06c6006a1b256ad4812436c258d9b6b8b4fcc
pavan <pavanpk0850@gmail.com>
2021-10-20 20:16:06 +0530

    fix page bg color

--- src/components/home/HomePages.js
+++ src/components/home/HomePages.js
@@ -21,24 +21,24 @@ const HomePages = ({ isMovie, isActive, setIsActive }) => {
 		<div className='flex items-center justify-center mb-4 group'>
 			<button
 				className={` ${
-					isActive === 1 && "bg-primary text-white"
-				} text-primary  border-l border-t border-b border-primary hover:bg-primary hover:text-white  font-bold uppercase text-xs px-4 py-2 rounded-l outline-none focus:outline-none mb-1 ease-linear transition-all duration-150  `}
+					isActive === 1 ? "bg-primary text-white" : "text-primary"
+				}  border-l border-t border-b border-primary hover:bg-primary hover:text-white  font-bold uppercase text-xs px-4 py-2 rounded-l outline-none focus:outline-none mb-1 ease-linear transition-all duration-150  `}
 				type='button'
 				onClick={(e) => btnHandler(e.currentTarget.innerHTML)}>
 				1
 			</button>
 			<button
 				className={` ${
-					isActive === 2 && "bg-primary text-white"
-				} text-primary  border border-solid border-primary hover:bg-primary hover:text-white  font-bold uppercase text-xs px-4 py-2 outline-none focus:outline-none mb-1 ease-linear transition-all duration-150'
+					isActive === 2 ? "bg-primary text-white" : "text-primary"
+				}    border border-solid border-primary hover:bg-primary hover:text-white  font-bold uppercase text-xs px-4 py-2 outline-none focus:outline-none mb-1 ease-linear transition-all duration-150'
 				type='button`}
 				onClick={(e) => btnHandler(e.currentTarget.innerHTML)}>
 				2
 			</button>
 			<button
 				className={`${
-					isActive === 3 && "bg-primary text-white"
-				} text-primary  border-t border-b border-r border-primary hover:bg-primary hover:text-white  font-bold uppercase text-xs px-4 py-2 rounded-r outline-none focus:outline-none mb-1 ease-linear transition-all duration-150`}
+					isActive === 3 ? "bg-primary text-white" : "text-primary"
+				}   border-t border-b border-r border-primary hover:bg-primary hover:text-white  font-bold uppercase text-xs px-4 py-2 rounded-r outline-none focus:outline-none mb-1 ease-linear transition-all duration-150`}
 				type='button'
 				onClick={(e) => btnHandler(e.currentTarget.innerHTML)}>
 				3
