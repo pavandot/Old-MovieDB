927592493d75af0bc7128c86432a90523f409f27
pavan <pavanpk0850@gmail.com>
2021-10-23 16:47:17 +0530

    fix

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -20,12 +20,24 @@ const Search = () => {
 					<h1 className='bg-blue-400 text-left h-16 p-5 font-bold  text-lg text-white rounded-t-lg'>Search Results</h1>
 					<ul className=' pt-3 spa'>
 						<li className={`flex justify-between px-5 py-3 ${isMovie && "bg-gray-100 "} cursor-pointer `} onClick={() => setIsMovie(true)}>
-							Movies <span className=' bg-white p-1 rounded-lg'>{movies.length}</span>
+							Movies <span className=' bg-white p-1 rounded-lg'>{!!movies && movies.length}</span>
 						</li>
 						<li className={`flex justify-between px-5 py-3 ${!isMovie && "bg-gray-100"} cursor-pointer rounded-b-lg`} onClick={() => setIsMovie(false)}>
-							Tv Shows <span className=' bg-white p-1 rounded-lg'>{tvShowsArr.length}</span>
+							Tv Shows <span className=' bg-white p-1 rounded-lg'>{!!tvShowsArr && tvShowsArr.length}</span>
 						</li>
 					</ul>
+				</div>
+				<div className='sm:hidden'>
+					<div className='m-5 sm:m-10 flex items-center'>
+						<div className='flex items-center mx-5 border-2 border-primary rounded-3xl  justify-between'>
+							<div className={`py-1 px-3 ${isMovie ? "rounded-3xl bg-primary text-white " : "rounded-tl-3xl rounded-bl-3xl bg-white text-gray-800"} cursor-pointer `} onClick={() => setIsMovie(true)}>
+								<p>Movies</p>
+							</div>
+							<div className={`py-1 px-4 ${!isMovie ? "rounded-3xl bg-primary text-white " : "rounded-tr-3xl rounded-br-3xl bg-white text-gray-800"}   cursor-pointer `} onClick={() => setIsMovie(false)}>
+								<p>TV</p>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div className=' sm:w-9/12 w-full'>
 					{isMovie &&
